920f8fd296eeb181be4849bc6e8ff4b577c35b76
Kwabena Boateng Konadu <officiallykbk@gmail.com>
2025-10-14 16:05:59 +0000

    fix: converted device crud test code to typescript

---
+++ src/tests/mockData.ts
@@ -0,0 +1,35 @@
+export const devices = [
+    {
+      "id": 1,
+      "name": "Updated Device A",
+      "type": "router",
+      "pingRate": null,
+      "latency": null,
+      "trafficLoad": null,
+      "status": "online",
+      "lastUpdated": new Date ("2025-10-13T17:16:36.040Z").toISOString(),
+      "scenarioId": 1
+    },
+    {
+      "id": 3,
+      "name": "Device A",
+      "type": "router",
+      "pingRate": null,
+      "latency": null,
+      "trafficLoad": null,
+      "status": "online",
+      "lastUpdated": new Date ("2025-10-13T17:24:33.186Z").toISOString(),
+      "scenarioId": 1
+    },
+    {
+      "id": 4,
+      "name": "Updated Device A",
+      "type": "router",
+      "pingRate": null,
+      "latency": null,
+      "trafficLoad": null,
+      "status": "online",
+      "lastUpdated": new Date ("2025-10-13T17:59:25.590Z").toISOString(),
+      "scenarioId": 1
+    }
+  ];
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,10 +1,10 @@
-export interface Device {
+export type Device = {
     id: number;
     name: string;
     type: string;        // router, switch, server
-    pingRate?: number;
-    latency?: number;
-    trafficLoad?: number;
+    pingRate?: number | null;
+    latency?: number | null;
+    trafficLoad?: number | null;
     status?: string;     // online/offline
     lastUpdated?: Date;
 }
