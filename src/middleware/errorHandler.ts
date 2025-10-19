730199284d73543a3fcca7f18858f1f8380e3eb8
Kwabena Boateng Konadu <officiallykbk@gmail.com>
2025-10-19 15:53:27 +0000

    chore: creating a global error handler

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction,  } from 'express';
 
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
   console.error(err.stack);
+  // err.statusCode = err.statusCode || 500;
+  // err.status = err.status || "error";
   const isProd = process.env.NODE_ENV === 'production';
   res.status(500).json({
     status: 500,
---
+++ src/utils/AppError.ts
@@ -0,0 +1,12 @@
+// import { Response } from "express";
+
+// class AppError extends Error {
+//   constructor(message: string, statusCode: string) {
+//     super(message);
+//     this.statusCode = statusCode;
+//     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+//     this.isOperational = true; // Distinguish handled vs unknown errors
+//   }
+// }
+
+// export default AppError;
